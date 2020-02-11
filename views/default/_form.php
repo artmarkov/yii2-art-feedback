c9b955653186fee08044a0e4afceb5b2d8a8372a
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-11 12:27:34 +0300

    Update _form.php

--- views/default/_form.php
+++ views/default/_form.php
@@ -13,98 +13,37 @@ use kartik\switchinput\SwitchInput;
 
 <div class="feedback-form">
 
-    <?php 
+    <?php
     $form = ActiveForm::begin([
-            'id' => 'feedback-form',
-            'validateOnBlur' => false,
-        ])
+        'id' => 'feedback-form',
+        'validateOnBlur' => false,
+    ])
     ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-8">
 
-    <div class="row">
-        <div class="col-md-8">
 
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-md-6"> 
-                            <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-                        </div>
-                        <div class="col-md-6"> 
-                            <?= $form->field($model, 'business')->textInput(['maxlength' => true]) ?>
-                        </div>
-                    </div>
+                    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
-                            <?= $form->field($model, 'content')->widget(trntv\aceeditor\AceEditor::class,
-                                     [
-                                         'mode' => 'html',
-                                         'theme' => 'sqlserver', //chrome,clouds,clouds_midnight,cobalt,crimson_editor,dawn,dracula,dreamweaver,eclipse,iplastic
-                                                                 //merbivore,merbivore_soft,sqlserver,terminal,tomorrow_night,twilight,xcode
-                                     ]) ?>
-                    
+                    <?= $form->field($model, 'business')->textInput(['maxlength' => true]) ?>
+
+
+                    <?= $form->field($model, 'content')->widget(trntv\aceeditor\AceEditor::class,
+                        [
+                            'mode' => 'html',
+                            'theme' => 'sqlserver', //chrome,clouds,clouds_midnight,cobalt,crimson_editor,dawn,dracula,dreamweaver,eclipse,iplastic
+                            //merbivore,merbivore_soft,sqlserver,terminal,tomorrow_night,twilight,xcode
+                        ]) ?>
+
+
                 </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['id'] ?>: </label>
-                            <span><?=  $model->id ?></span>
-                        </div>
-                        
-                        <?php if (!$model->isNewRecord): ?>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                         <?= $model->attributeLabels()['created_at'] ?> :
-                            </label>
-                            <span><?= $model->createdDatetime ?></span>
-                        </div>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                        <?= $model->attributeLabels()['updated_at'] ?> :
-                            </label>
-                            <span><?= $model->updatedDatetime ?></span>
-                        </div>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                        <?= $model->attributeLabels()['updated_by'] ?> :
-                            </label>
-                            <span><?= $model->updatedBy->username ?></span>
-                        </div>
-                        
-                        <?php endif; ?>
-                           
-                        <div class="form-group">
-                            <?php  if ($model->isNewRecord): ?>
-                                <?= Html::submitButton(Yii::t('art', 'Create'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Cancel'), ['/feedback/default/index'], ['class' => 'btn btn-default']) ?>
-                            <?php  else: ?>
-                                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Delete'),
-                                    ['/feedback/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-danger',
-                                    'data' => [
-                                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'method' => 'post',
-                                    ],
-                                ]) ?>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-body">
+                <div class="col-md-4">
                     <?= $form->field($model, 'published_at')
-                            ->widget(DatePicker::className(), ['dateFormat' => 'yyyy-MM-dd', 'options' => ['class' => 'form-control']]);
+                        ->widget(DatePicker::className(), ['dateFormat' => 'yyyy-MM-dd', 'options' => ['class' => 'form-control']]);
                     ?>
-                    
+
                     <?= $form->field($model, 'status')->dropDownList(Feedback::getStatusList()) ?>
 
                     <?= $form->field($model, 'main_flag')->widget(SwitchInput::classname(), [
@@ -117,8 +56,25 @@ use kartik\switchinput\SwitchInput;
                 </div>
             </div>
         </div>
+        <div class="panel-footer">
+            <div class="form-group">
+                    <?= Html::a(Yii::t('art', 'Go to list'), ['/feedback/default/index'], ['class' => 'btn btn-default']) ?>
+                    <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
+                <?php if (!$model->isNewRecord): ?>
+                    <?= Html::a(Yii::t('art', 'Delete'),
+                        ['/feedback/default/delete', 'id' => $model->id], [
+                            'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                <?php endif; ?>
+            </div>
+            <?= \artsoft\widgets\InfoModel::widget(['model' => $model]); ?>
+        </div>
     </div>
 
-    <?php  ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 
 </div>
